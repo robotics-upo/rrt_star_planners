7744cb1991176fe9423dad67f4577c9411ddac09
david-alejo <churr.te@gmail.com>
2024-10-11 15:20:27 +0200

    Fixed script for obtaining RRT comparison. Pending to do PDP and CDP integration

--- script/catenary_comparison_tests.bash
+++ script/catenary_comparison_tests.bash
@@ -12,51 +12,41 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-# Setup the environment
-TARGET=$HOME/catenary_stats
-mkdir -p $TARGET
-
 # Add a number of tests --> this is not necessary: 
 CONTADOR=$1
 until [ $CONTADOR -gt $2 ]; do
-    roslaunch rrt_planners test_uav.launch map_file:=stage_${CONTADOR} &
-    roslaunch rrt_planners random_planner_uav.launch map:=stage_$CONTADOR &
-    sleep 7
-    cd $HOME/marsupial_ws/src/marsupial_optimizer/trees/script/ && ./execute_tree.sh stage_$CONTADOR.xml
-    rostopic echo -n 3 /behavior_tree/execution_status   # Wait for the response before the next
+    roslaunch rrt_planners random_planner_marsupial.launch map:=stage_$CONTADOR &
+    sleep 20
+    cd $HOME/marsupial_ws/src/marsupial_optimizer/trees/script/ && ./execute_tree.sh stage_${CONTADOR}.xml
+    rostopic echo -n 1 /behavior_tree/load_tree_action/result  # Wait for the response before the next
     rosnode kill /random_planner_node
-    sleep 5
-    mv $HOME/.ros/rrt_stats.txt $TARGET/rrt_stats_bisection_$CONTADOR.txt
-
-    # Second problem, bisection
-    roslaunch rrt_planners random_global_uav_planner.launch map_file:=stage_$CONTADOR &
+    sleep 15
+     
+    # # # Second problem, bisection
+    roslaunch rrt_planners random_planner_marsupial.launch map:=stage_$CONTADOR num_pos_initial:="2" &
+    sleep 20
     cd $HOME/marsupial_ws/src/marsupial_optimizer/trees/script/ && ./execute_tree.sh stage_${CONTADOR}_2.xml
-    rostopic echo -n 3 /behavior_tree/execution_status
+    rostopic echo /behavior_tree/load_tree_action/result -n 1
     rosnode kill /random_planner_node
-    sleep 5
-    mv $HOME/.ros/rrt_stats.txt $TARGET/rrt_stats_bisection_${CONTADOR}_2.txt
+    sleep 15
 
-    # Parable approximation
-    roslaunch rrt_planners random_global_uav_planner.launch map_file:=stage_$CONTADOR use_parable:=true &
-    sleep 7
+    # # Parabola approximation
+    roslaunch rrt_planners random_planner_marsupial.launch map:=stage_$CONTADOR use_parable:=true &
+    sleep 20
+    cd $HOME/marsupial_ws/src/marsupial_optimizer/trees/script/ && ./execute_tree.sh stage_${CONTADOR}.xml
+    rostopic echo -n 1 /behavior_tree/load_tree_action/result
+    rosnode kill /random_planner_node
+    sleep 15
+
+    #Second problem parabola
+    roslaunch rrt_planners random_planner_marsupial.launch map:=stage_$CONTADOR use_parable:=true num_pos_initial:="2" &
+    sleep 20
     cd $HOME/marsupial_ws/src/marsupial_optimizer/trees/script/ && ./execute_tree.sh stage_$CONTADOR.xml
-    rostopic echo -n 3 /behavior_tree/execution_status
+    rostopic echo -n 1 /behavior_tree/load_tree_action/result
     rosnode kill /random_planner_node
-    sleep 5
-    mv $HOME/.ros/rrt_stats.txt $TARGET/rrt_stats_parable_$CONTADOR.txt
-    mv $HOME/.ros/catenary_stats.txt $TARGET/precomputing_parable_catenary_stats_${CONTADOR}.txt
-
-    #Second problem parable
-    roslaunch rrt_planners random_global_uav_planner.launch map_file:=stage_$CONTADOR use_parable:=true &
-    sleep 7
-    cd $HOME/marsupial_ws/src/marsupial_optimizer/trees/script/ && ./execute_tree.sh stage_${CONTADOR}_2.xml
-    rostopic echo -n 3 /behavior_tree/execution_status
-    rosnode kill /random_planner_node
-    sleep 5
-    mv $HOME/.ros/rrt_stats.txt $TARGET/rrt_stats_parable_${CONTADOR}_2.txt
-    mv $HOME/.ros/catenary_stats.txt $TARGET/precomputing_parable_catenary_stats_${CONTADOR}_2.txt
-
-    # Comparison parable/
+    sleep 15
+ 
+    # Comparison parable/catenary runtime TODO: integrate!
 #    roslaunch rrt_planners random_global_uav_planner.launch map_file:=stage_$CONTADOR use_both:=true &
     #sleep 7
 #    cd $HOME/marsupial_ws/src/marsupial_optimizer/trees/script/ && ./execute_tree.sh stage_$CONTADOR.xml
@@ -73,9 +63,9 @@ until [ $CONTADOR -gt $2 ]; do
 #    sleep 5
 #    mv $HOME/.ros/catenary_stats.txt $TARGET/catenary_stats_${CONTADOR}_2.txt
 
-    echo Performed Tests Contador = $CONTADOR
+    echo Performed Tests of Stage = $CONTADOR
     rosnode kill -a
-    sleep 20
+    sleep 2
 
     let CONTADOR+=1
 done
